5aec75c781f8173803b83f0229ffaeee3dab21ec
ardeche2000 <mokokoardeche@gmail.com>
2024-02-24 17:37:05 +0100

    food-projet

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import React, { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isLoaded] = useFonts({
+    "mrt-mid": require("./assets/fonts/Kanit-Medium.ttf"),
+  });
+  const handleOnLayout = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync(); //hide the splashscreen
+    }
+  }, [isLoaded]);
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={handleOnLayout}>
+      <Text style={styles.textOne}>Bonjour</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +34,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textOne:{
+    fontFamily:"mrt-mid",
+    fontSize:25
+  }
 });
